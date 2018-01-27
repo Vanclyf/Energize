898371b365839fbc300492fa0e2a1f3cd1d2c7f8
leeuwk004 <lion.drie@gmail.com>
2018-01-27 14:17:41 +0100

    Made resources and more shit

---
+++ Energize/Assets/Scripts/PowerUpManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour {
+    [SerializeField] private float coolDownTimer;
+
+    const float _MIN_SPAWN_DELAY = 25f;
+    const float _MAX_SPAWN_DELAY = 40f;
+    GameObject WattRegen;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        coolDownTimer += Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (coolDownTimer >= Random.Range(_MIN_SPAWN_DELAY, _MAX_SPAWN_DELAY))
+        {
+            WattRegen = Instantiate(Resources.Load("turnUp4Watt")) as GameObject;
+            WattRegen.GetComponent<WattRegenPower>()._Spawn();
+            coolDownTimer = 0;
+        }
+    }
+}
--- Energize/Assets/Scripts/WattRegenPower.cs
+++ Energize/Assets/Scripts/WattRegenPower.cs
@@ -7,43 +7,22 @@ public class WattRegenPower : MonoBehaviour {
     private int wattIncrease, maxWatt;
     public bool onCooldown;
     private WattBarManager WattBarManager;
-    private float coolDownTimer;
-   
-    const float _FIRST_SPAWN_DELAY = 20f;
-    const float _MIN_SPAWN_DELAY = 25f;
-    const float _MAX_SPAWN_DELAY = 40f;
+
 
 
     // Use this for initialization
     void Start () {
         InitializeVariable();
-        InitializeCooldown();
 	}
 
-    private void InitializeCooldown()
-    {
-        coolDownTimer += Time.deltaTime;
-        if (coolDownTimer >= _FIRST_SPAWN_DELAY)
-        {
-            _Spawn();
-            
-        }
-    }
+  
 
     private void InitializeVariable(){
         wattIncrease = 30;
         maxWatt = 100;
-        this.gameObject.SetActive(false);
-        WattBarManager = player.GetComponent<WattBarManager>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        coolDownTimer += Time.deltaTime ;
-        if (coolDownTimer >= Random.Range(_MIN_SPAWN_DELAY, _MAX_SPAWN_DELAY)){
-            _Spawn();
-        }
+        player = GameObject.Find("playerTemp");
+        WattBarManager = player.GetComponent<WattBarManager>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -62,25 +41,21 @@ public class WattRegenPower : MonoBehaviour {
                 WattBarManager.GainWatt(wattIncrease);
             }
 
-            this.gameObject.SetActive(false);
+            GameObject.Destroy(this);
             onCooldown = true;
 
 
         }
     }
 
-    private void _Spawn()
+    public void _Spawn()
     {
-        coolDownTimer = 0;
-     
+            
         Vector3 pos = new Vector3(Random.Range(0.2f , 0.8f), Random.Range(1.1f, 1.3f ), 0);
         gameObject.GetComponent<Transform>().localPosition = Camera.main.ViewportToWorldPoint(pos);
         //Vector3 playerPos = player.GetComponent<Transform>().localPosition;
-        this.gameObject.SetActive(true);
         
-        
-
-
+        onCooldown = false;
 
     }
 

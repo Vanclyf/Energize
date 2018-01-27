e21f94b272a63fbd95e739224696a1748b30e944
Darryl van Denzel <darryl.van.denzel@hva.nl>
2018-01-27 14:16:24 +0100

    adds swipe movement for the lightning bolt

--- Energize/Assets/LightningMoveScript.cs
+++ Energize/Assets/LightningMoveScript.cs
@@ -8,30 +8,56 @@ public class LightningMoveScript : MonoBehaviour {
     public GameObject obj1, obj2;
     public Transform trans1, trans2, trans3, trans4;
     public float time;
+    Rigidbody2D m_Rigidbody;
+    Vector3 dir;
+    float vel;
+    int i = 1;
+    public bool moving;
     public Text text;
 
-    int i = 1;
+
 
 
     // Use this for initialization
     void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        text.text = "X = " + obj2.transform.position.x + "Y = " + obj2.transform.position.y + "X = " + obj2.transform.position.z;
+        m_Rigidbody = GetComponent<Rigidbody2D>();
 
     }
 
-    public void ButtonForMove(Vector3 posTarget)
-    {
-        obj2.transform.position = new Vector3(posTarget.x, posTarget.y, 1);
+    // Update is called once per frame
+    void Update () {
+        if (moving)
+        {
+            transform.position += dir * vel * Time.deltaTime;
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
 
+        }
+
+    }
+
+    public void ButtonForMove(Vector2 direction, float velocity)
+    {
+        dir = direction;
+        vel = velocity;
+        //obj2.transform.position = new Vector3(posTarget.x, posTarget.y, 1);
+        m_Rigidbody.velocity = transform.forward * velocity;
+        moving = true;
     }
 
     public void MoveObject()
     {
         iTween.MoveTo(obj1, obj2.transform.position, time);
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        text.text = "COLLOOOOOOOOO";
+
+        MoveObject();
+        moving = false;
     }
 }
--- Energize/Assets/Scripts/PlayerController.cs
+++ Energize/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
     private int speed = 5;
@@ -10,74 +11,136 @@ public class PlayerController : MonoBehaviour {
     private float dragDistance;  //minimum distance for a swipe to be registered
     private Vector3 pos;
     public LightningMoveScript lightningMoveScript;
+    private Vector2 nextPosition;
+    private bool isMoving;
+    public GameObject anchorPointsParent;
+    Transform[] anchorPointsArray;
+    private Vector2 dir;
+    bool inAir = false;
+
 
     // Use this for initialization
     void Start () {
         initializeVariables();
+        lightningMoveScript = GetComponent<LightningMoveScript>();
+
     }
 
     private void initializeVariables()
     {
         pos = transform.position;
         dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+        Vector2 nextPosition = new Vector2();
+        anchorPointsArray = new Transform[anchorPointsParent.transform.childCount];
+        for(int i = 0; i < anchorPointsArray.Length; i++)
+        {
+            anchorPointsArray[i] = anchorPointsParent.transform.GetChild(i);
+        }
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        pos = transform.position;
-        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        if(inAir == true)
         {
-            Touch touch = Input.GetTouch(0); // get the touch
-            if (touch.phase == TouchPhase.Began) //check for the first touch
+            inAir = lightningMoveScript.moving;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isMoving = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isMoving = false;
+        }
+        if(isMoving == true)
+        {
+            gotoPosition(nextPosition);
+        }
+        pos = transform.position;
+
+        if(inAir == false)
+        {
+            if (Input.touchCount == 1) // user is touching the screen with a single touch
             {
-                fp = touch.position;
-                lp = touch.position;
-            }
-            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
-            {
-                lp = touch.position;
-                Vector3 Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                lightningMoveScript.ButtonForMove(Pos);
 
-            }
-            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
-            {
-                lp = touch.position;  //last touch position. Ommitted if you use list
-                lightningMoveScript.MoveObject();
+                Touch touch = Input.GetTouch(0); // get the touch
+                if (touch.phase == TouchPhase.Began) //check for the first touch
+                {
+                    //lightningMoveScript.();
+                    fp = touch.position;
+                    lp = touch.position;
+                }
+                else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+                {
+                    lp = touch.position;
+                    Vector3 Pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 
+                }
+                else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+                {
 
-                //Check if drag distance is greater than 20% of the screen height
-                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
-                {//It's a drag
-                
-                    pos = Camera.main.ScreenToWorldPoint(lp);
-                    pos.z = transform.position.z;
+                    lp = touch.position;  //last touch position. Ommitted if you use list
 
 
+                    /*for (int i = 0; i < anchorPointsArray.Length; i++)
+                    {
+                        text.text = "In For" + i;
+
+                        if (anchorPointsArray[i].position.x - lp.x < anchorPointsArray[i + 1].position.x - lp.x &&
+                            anchorPointsArray[i].position.y - lp.y < anchorPointsArray[i + 1].position.y - lp.y)
+                        {
+                            text.text = "in if" + i;
+
+                            lightningMoveScript.ButtonForMove(anchorPointsArray[i].position);
+                            text.text = "End If" + i;
+
+
+                        }
+
+                    }*/
+
+
+                    dir = lp - fp;
+                    dir.Normalize();
+
+                    lightningMoveScript.ButtonForMove(dir, 250);
+                    inAir = true;
+
+
+                    //Check if drag distance is greater than 20% of the screen height
+                    if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                    {//It's a drag
+
+                        pos = Camera.main.ScreenToWorldPoint(lp);
+                        pos.z = transform.position.z;
+
+
+                    }
+                    else
+                    {   //It's a tap as the drag distance is less than 20% of the screen height
+                        Debug.Log("Tap");
+
+                    }
                 }
                 else
-                {   //It's a tap as the drag distance is less than 20% of the screen height
-                    Debug.Log("Tap");
+                {
 
                 }
             }
-            else
-            {
 
+                /** if (Input.touchCount > 0)
+                 {
+                     Vector2 touchPosition = Input.GetTouch(0).position;
+                     touchPosition.x = this.transform.position.x - Camera.main.transform.position.x;
+                     touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+                     touchPosition.y = this.transform.position.y;
+                     this.transform.position = Vector2.MoveTowards(this.transform.position, touchPosition, Time.deltaTime * speed);
+                 }**/
             }
 
-
-            /** if (Input.touchCount > 0)
-             {
-                 Vector2 touchPosition = Input.GetTouch(0).position;
-                 touchPosition.x = this.transform.position.x - Camera.main.transform.position.x;
-                 touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-                 touchPosition.y = this.transform.position.y;
-                 this.transform.position = Vector2.MoveTowards(this.transform.position, touchPosition, Time.deltaTime * speed);
-             }**/
-        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -93,6 +156,32 @@ public class PlayerController : MonoBehaviour {
        
     }
 
-    
+    private void gotoPosition(Vector2 pos)
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, pos, step);
+    }
+
+
+    public GameObject FindClosestAnchor()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Anchorpoint");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
 }
 
